271808ce1840878a63515dc7c49c7003a58abc9d
touchscale <11134128+touchscale@user.noreply.gitee.com>
2023-03-16 21:50:30 +0800

    update apps/Xijie/Xijie.js

--- apps/Xijie/Xijie.js
+++ apps/Xijie/Xijie.js
@@ -69,14 +69,14 @@ export class Xijie extends plugin {
 		let lastxijie_time = await redis.get(
 			'xiuxian:player:' + usr_qq + ':lastxijie_time'
 		);
-		lastxijie_time = parseInt(lastxijie_time);
-		if (nowTime < lastxijie_time + 5400000) {
-			let lastxijie_m = Math.trunc((lastxijie_time + 5400000 - nowTime) / 60 / 1000);
+		let cd123 = 60000 * 360;
+		if (nowTime < lastxijie_time + cd123) {
+			let lastxijie_m = Math.trunc((lastxijie_time + cd123 - nowTime) / 60 / 1000);
 			let lastxijie_s = Math.trunc(
-				((lastxijie_time + 5400000 - nowTime) % 60000) / 1000
+				((lastxijie_time + cd123 - nowTime) % 60000) / 1000
 			);
 			e.reply(
-				`每90分钟洗劫一次，正在CD中，` + `剩余cd: ${lastxijie_m}分${lastxijie_s}秒`
+				`每6小时洗劫一次，正在CD中，` + `剩余cd: ${lastxijie_m}分${lastxijie_s}秒`
 			);
 			return;
 		}
@@ -112,7 +112,7 @@ export class Xijie extends plugin {
 		}
 
 		//开始准备洗劫
-		player.魔道值 -= (50 * shop[i].Grade);
+		player.魔道值 -= 50 * shop[i].Grade;
 		await Write_player(usr_qq, player);
 
 		shop[i].state = 1;
