78efe6759961f4e75c023a41e2c01b43cad93416
闹钟不闹 <13612471008@163.com>
2023-03-16 14:34:18 +0000

    update apps/Occupation/Occupation.js.
    Signed-off-by: 闹钟不闹 <13612471008@163.com>

--- apps/Occupation/Occupation.js
+++ apps/Occupation/Occupation.js
@@ -624,7 +624,7 @@ export class Occupation extends plugin {
 			'魔蕴花',
 			'仙蕴花',
 			'仙缘草',
-			'太玄仙草',
+			'太玄仙草'
 		];
 		const sum2 = [0.1, 0.1, 0.1, 0.1, 0.1, 0, 0, 0, 0, 0, 0];
 		const sum3 = [
